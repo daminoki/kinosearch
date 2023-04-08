25d8175b0e364f06722ae426814c69377df7a927
Daria Andreeva <dd1556@mail.ru>
2023-04-08 19:36:58 +0300

    add header component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,12 @@
 import React from 'react';
-import axios from 'axios';
 
+import './assets/styles/reset.scss';
+import './assets/styles/variables.scss';
 import './App.css';
 
 import Header from './components/Header';
 
-
 function App() {
-  const getRandomFilm = async () => {
-    try {
-      const data = await axios.get('https://api.kinopoisk.dev/v1/movie/random', {
-        headers: {
-          'x-api-key': '26VY6W0-4BRMYG8-MY8VVQ7-SVJRV2V'
-        }
-      });
-      console.log(data);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  React.useEffect(() => {
-    getRandomFilm();
-  });
 
   return (
     <Header />
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 
+import styles from './Header.module.scss';
+
 const Header = () => {
   return (
-    <div>
-      <div></div>
-    </div> 
+    <header className={styles.header}>
+      <div className={styles.header__container}>
+        <img className={styles.header__logo} src='./img/logo.svg' />
+        <input className={styles.header__search} type='search' placeholder='Название фильма, сериала, актера или режиссера' />
+      </div>
+    </header> 
   );
 };
 
