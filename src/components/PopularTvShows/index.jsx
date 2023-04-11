e5abc057c336b2d6d82b505bb03d964bdaa6e0c6
Daria Andreeva <dd1556@mail.ru>
2023-04-11 10:10:23 +0300

    add styles and swiper api to popular tv show comp

--- src/components/PopularTvShows/index.jsx
+++ src/components/PopularTvShows/index.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import axios from 'axios';
+import Swiper from 'swiper/bundle';
 
 import styles from './PopularTvShows.module.scss';
+import 'swiper/css/bundle';
 
 const PopularTvShows = () => {
   const [shows, setShows] = React.useState([]);
@@ -13,8 +15,7 @@ const PopularTvShows = () => {
           'x-api-key': '26VY6W0-4BRMYG8-MY8VVQ7-SVJRV2V'
         }
       });
-      setShows(data);
-      console.log(shows);
+      setShows(data.docs);
     } catch (error) {
       console.log(error);
     }
@@ -28,18 +29,28 @@ const PopularTvShows = () => {
     fetchData();
   }, []);
 
+  // eslint-disable-next-line no-unused-vars
+  const showsSlider = new Swiper('#shows-slider', {
+    loop: true,
+    spaceBetween: '10',
+  });
+
   return (
     <div className={styles['popular-shows']}>
       <p className={styles['popular-shows__title']}>Популярные сериалы</p>
-      <ul className={styles['popular-shows__list']}>
-        {/* {shows.map((show, index) => (
-          <li
-            key={index}
-            className={styles['popular-shows__item']}
-          >
-            {show}
-          </li>
-        ))} */}
+      <ul className={'swiper' + ' ' + styles['popular-shows__list']} id='shows-slider'>
+        <div className='swiper-wrapper'>
+          {shows.map((show) => (
+            <li key={show.id} className={'swiper-slide' + ' ' + styles['popular-shows__item']}>
+              <img src={show.poster.previewUrl} alt='Постер' width='150' height='225' />
+              <span className={styles['popular-shows__name']}>{show.name}</span>
+              <div className={styles['popular-shows__details']}>
+                <span className={styles['popular-shows__release-year']}>{show.releaseYears[0].start}-{show.releaseYears[0].end ? show.releaseYears[0].end : '...'}, </span>
+                <span className={styles['popular-shows__type']}>{show.genres[0].name}</span>
+              </div>
+            </li>
+          ))}
+        </div>
       </ul>
     </div>
   );
