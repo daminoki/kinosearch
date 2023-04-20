c85fd80ab50271cd80ee2f9159b0c16fd66050a7
Daria Andreeva <dd1556@mail.ru>
2023-04-20 11:14:17 +0300

    fix buttons in popular tv shows section

--- src/components/PopularTvShows/index.jsx
+++ src/components/PopularTvShows/index.jsx
@@ -31,12 +31,11 @@ const PopularTvShows = () => {
 
   // eslint-disable-next-line no-unused-vars
   const showsSlider = new Swiper('#shows-slider', {
-    loop: true,
     spaceBetween: '10',
 
     navigation: {
-      nextEl: '.popular-shows__next-btn',
-      prevEl: '.popular-shows__prev-btn',
+      nextEl: '#popular-shows__next-btn',
+      prevEl: '#popular-shows__prev-btn',
     },
   });
 
@@ -56,8 +55,16 @@ const PopularTvShows = () => {
             </li>
           ))}
         </div>
-        <button className={styles['popular-shows__prev-btn']}></button>
-        <button className={styles['popular-shows__next-btn']}></button>
+        <button className={styles['popular-shows__prev-btn']} id="popular-shows__prev-btn">
+          <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24">
+            <path fill="currentColor" d="m14 18l-1.4-1.45L16.15 13H4v-2h12.15L12.6 7.45L14 6l6 6l-6 6Z"/>
+          </svg>
+        </button>
+        <button className={styles['popular-shows__next-btn']} id="popular-shows__next-btn">
+          <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24">
+            <path fill="currentColor" d="m14 18l-1.4-1.45L16.15 13H4v-2h12.15L12.6 7.45L14 6l6 6l-6 6Z"/>
+          </svg>
+        </button>
       </ul>
     </div>
   );
