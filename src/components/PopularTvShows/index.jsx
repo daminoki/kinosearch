02f0b19bce476174415ba179b2dc68c45af022d0
Daria Andreeva <dd1556@mail.ru>
2023-06-01 20:51:40 +0300

    coderefactoring, added svg sprite component

--- src/components/PopularTvShows/index.jsx
+++ src/components/PopularTvShows/index.jsx
@@ -3,6 +3,7 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import { SwiperButtonNext, SwiperButtonPrev } from '../../utils/SwiperNavigationBtns';
 
 import api from '../../api';
+import Loader from '../Loader';
 
 import styles from './PopularTvShows.module.scss';
 import 'swiper/css';
@@ -33,10 +34,7 @@ const PopularTvShows = () => {
   return (
     <div className={styles['popular-shows']}>
       <p className={styles['popular-shows__title']}>Популярные сериалы</p>
-      {!shows && 
-        <div className={styles['center-body']}>
-          <div className={styles['loader-circle-2']}></div>
-        </div>}
+      {!shows && <Loader />}
       {shows && 
         <Swiper
           spaceBetween={20}
@@ -55,15 +53,15 @@ const PopularTvShows = () => {
           ))}
           <SwiperButtonNext>
             <div className={styles['popular-shows__next-btn']}>
-              <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24">
-                <path fill="currentColor" d="m14 18l-1.4-1.45L16.15 13H4v-2h12.15L12.6 7.45L14 6l6 6l-6 6Z"/>
+              <svg width="32" height="32">
+                <use xlinkHref="#arrow"></use>
               </svg>
             </div>
           </SwiperButtonNext>
           <SwiperButtonPrev>
             <div className={styles['popular-shows__prev-btn']}>
-              <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24">
-                <path fill="currentColor" d="m14 18l-1.4-1.45L16.15 13H4v-2h12.15L12.6 7.45L14 6l6 6l-6 6Z"/>
+              <svg width="32" height="32">
+                <use xlinkHref="#arrow"></use>
               </svg>
             </div>
           </SwiperButtonPrev>
