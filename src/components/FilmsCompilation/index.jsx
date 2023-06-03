ff2af097e64ea3bb9715ba5ca09c26b7aebaa1fd
Daria Andreeva <dd1556@mail.ru>
2023-06-03 21:07:56 +0300

    add styles to films list component

--- src/components/FilmsCompilation/index.jsx
+++ src/components/FilmsCompilation/index.jsx
@@ -7,7 +7,11 @@ function FilmsCompilaition () {
   return (
     <ul className={styles['films-compilation']}>
       <li>
-        <Link to='/films/top250' className={styles['films-compilation__item']}>
+        <Link to='/films/top250' className={styles['films-compilation__item']} state={{
+          title: '250 лучших фильмов', 
+          description: 'Рейтинг составлен по результатам голосования посетителей сайта. Любой желающий может принять в нем участие, проголосовав за свой любимый фильм.', 
+          img: '../img/top250.png'
+        }}>
           <img src='./img/top250.png' width='88' height='88' alt='Топ 250 фильмов'/>
           <div className={styles['films-compilation__info']}>
             <p className={styles['films-compilation__title']}>250 лучших фильмов</p>
@@ -21,6 +25,19 @@ function FilmsCompilaition () {
           <div className={styles['films-compilation__info']}>
             <p className={styles['films-compilation__title']}>500 лучших фильмов</p>
             <p className={styles['films-compilation__count']}>500 фильмов</p>
+          </div>
+        </Link>
+      </li>
+      <li>
+        <Link to='/films/nolan' className={styles['films-compilation__item']} state={{
+          title: 'Фильмы Кристофера Нолана', 
+          description: 'Кристофер Нолан — британский и американский кинорежиссёр, сценарист и продюсер. Один из самых кассовых режиссёров в истории, а также один из самых известных и влиятельных кинематографистов.', 
+          img: '../img/nolan.jpeg'
+        }}>
+          <img src='./img/nolan.jpeg' width='88' height='88' alt='Кристофер Нолан'/>
+          <div className={styles['films-compilation__info']}>
+            <p className={styles['films-compilation__title']}>Фильмы Кристофера Нолана</p>
+            <p className={styles['films-compilation__count']}>Фильмы британского и американского кинорежиссёра, сценариста и продюсера</p>
           </div>
         </Link>
       </li>
--- src/components/FilmsList/index.jsx
+++ src/components/FilmsList/index.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
-import { Link, useParams } from 'react-router-dom';
+import { Link, useParams, useLocation } from 'react-router-dom';
 
 import api from '../../api';
 
 import BaseLayout from '../BaseLayout';
+import Loader from '../Loader';
 import styles from './FilmsList.module.scss';
 
-function FilmsList ({ title }) {
+function FilmsList () {
   const {id} = useParams();
+  const { state } = useLocation();
 
   const [films, setFilms] = React.useState(null);
 
@@ -31,6 +33,10 @@ function FilmsList ({ title }) {
     fetchData();
   }, []);
 
+  const getValue = (value) => {
+    return value ? value : '-';
+  };
+
   console.log(films);
 
   return (
@@ -38,7 +44,40 @@ function FilmsList ({ title }) {
       <Link to='/films'>
         <p className={styles['films-list__back']}>Все списки</p>
       </Link>
-      <p className={styles['films-list__title']}>{title}</p>
+      <div className={styles['films-list__header']}>
+        <div className={styles['films-list__column']}>
+          <p className={styles['films-list__title']}>{state.title}</p>
+          <p className={styles['films-list__description']}>{state.description}</p>
+        </div>
+        <div className={styles['films-list__img']}>
+          <img src={state.img} alt={state.title} width='160' height='160'></img>
+        </div>
+      </div>
+      <div className={styles['films-list__body']}>
+        {!films && <Loader />}
+        {films &&
+          <ul className={styles['films-list__list']}>
+            {films.map((film) => (
+              <li key={film.id} className={styles['films-list__item']}>
+                <span className={styles['films-list__count']}>{films.indexOf(film) + 1}</span>
+                <div className={styles['films-list__info']}>
+                  <div className={styles['films-list__poster']}>
+                    <img src={film.poster.url} alt={film.name}></img>
+                  </div>
+                  <div className={styles['films-list__main-info']}>
+                    <p className={styles['films-list__name']}>{film.name}</p>
+                    <div className={styles['films-list__secondary-info']}>
+                      <span className={styles['films-list__year']}>{`${getValue(film.alternativeName)}, ${film.year}, ${getValue(film.movieLength)} мин.`}</span>
+                      <span className={styles['films-list__country']}>{`${film.countries[0].name}, ${film.genres[0].name}`}</span>
+                    </div>
+                    <p className={styles['films-list__summary']}>{film.shortDescription}</p>
+                    <p className={styles['films-list__rate']}>{`Кинопоиск: ${film.rating.kp}, IMBD: ${film.rating.imbd}`}</p>
+                  </div>
+                </div>
+              </li>
+            ))}
+          </ul>}
+      </div>
     </BaseLayout>
   );
 }
