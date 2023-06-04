04722e9b9fc0b3dafdb1d4be08d036a98132ca1f
Daria Andreeva <dd1556@mail.ru>
2023-06-04 17:06:23 +0300

    add functionality of rendering different type of lists

--- src/components/FilmsList/index.jsx
+++ src/components/FilmsList/index.jsx
@@ -8,16 +8,19 @@ import Loader from '../Loader';
 import styles from './FilmsList.module.scss';
 
 function FilmsList () {
-  const {id} = useParams();
+  const { id } = useParams();
   const { state } = useLocation();
+  console.log(id);
 
   const [films, setFilms] = React.useState(null);
 
   const searchParams = {
     page: '1',
     limit: '50',
-    type: 'movie',
-    [id]: '!null',
+    top250: state.top250,
+    'persons.id': state.person,
+    top10: state.top10,
+    type: state.type
   };
 
   const fetchFilms = async () => {
@@ -34,7 +37,23 @@ function FilmsList () {
   }, []);
 
   const getValue = (value) => {
-    return value ? value : '-';
+    return (value && value !== null) ? value : '-';
+  };
+
+  const getFilmPoster = (value) => {
+    return (value && value !== null) ? value.url : '../img/placeholder.png';
+  };
+
+  const getFilmName = (value, film) => {
+    return (value && value !== null) ? value : film.alternativeName;
+  };
+
+  const getFilmList = (countriesArray) => {
+    return (
+      countriesArray.map((country, index) => (
+        <span key={index}>{country.name}{index != (countriesArray.length - 1) ? ',' : ''} </span>
+      ))
+    );
   };
 
   console.log(films);
@@ -62,16 +81,17 @@ function FilmsList () {
                 <span className={styles['films-list__count']}>{films.indexOf(film) + 1}</span>
                 <div className={styles['films-list__info']}>
                   <div className={styles['films-list__poster']}>
-                    <img src={film.poster.url} alt={film.name}></img>
+                    <img src={getFilmPoster(film.poster)} alt={film.name}></img>
                   </div>
                   <div className={styles['films-list__main-info']}>
-                    <p className={styles['films-list__name']}>{film.name}</p>
+                    <p className={styles['films-list__name']}>{getFilmName(film.name, film)}</p>
                     <div className={styles['films-list__secondary-info']}>
                       <span className={styles['films-list__year']}>{`${getValue(film.alternativeName)}, ${film.year}, ${getValue(film.movieLength)} мин.`}</span>
-                      <span className={styles['films-list__country']}>{`${film.countries[0].name}, ${film.genres[0].name}`}</span>
+                      <span className={styles['films-list__country']}>{getFilmList(film.countries)}</span>
+                      <span className={styles['films-list__genres']}>{getFilmList(film.genres)}</span>
                     </div>
                     <p className={styles['films-list__summary']}>{film.shortDescription}</p>
-                    <p className={styles['films-list__rate']}>{`Кинопоиск: ${film.rating.kp}, IMBD: ${film.rating.imbd}`}</p>
+                    <p className={styles['films-list__rate']}>{`Кинопоиск: ${film.rating.kp}, IMDB: ${film.rating.imdb}`}</p>
                   </div>
                 </div>
               </li>
